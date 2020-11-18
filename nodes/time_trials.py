d9fcf0faf878d424fe26e9a9e791ae7ba84bdf78
wecassidy <wec.notif.github@gmail.com>
2020-11-18 01:08:59 -0500

    Start, wait 1s, then stop the clock

--- nodes/time_trials.py
+++ nodes/time_trials.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python2
 
-print("Hello, world!")
+from collections import namedtuple
+import time
+
+import rospy
+import std_msgs
+
+topics = namedtuple("Topics", ("camera", "clock", "drive", "plates"))(
+    "/RP1/pi_camera/image_raw", "/clock", "/R1/cmd_vel", "/license_plate"
+)
+
+
+def plate_message(location, plate_number):
+    """Format a message for the license plate scorer"""
+    return "Hippos,1234,{},{}".format(location, plate_number)
+
+
+if __name__ == "__main__":
+    rospy.init_node("time_trials", anonymous=True)
+    plates = rospy.Publisher(topics.plates, std_msgs.msg.String, queue_size=1)
+
+    time.sleep(1)
+
+    plates.publish(plate_message(0, "AA00"))
+    rospy.sleep(1)
+    plates.publish(plate_message(-1, "AA00"))
