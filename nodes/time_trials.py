e215ada330432785fe6fdffdd820bae097a37303
wecassidy <wec.notif.github@gmail.com>
2020-11-18 01:33:18 -0500

    Drive around blindly

--- nodes/time_trials.py
+++ nodes/time_trials.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 import time
 
 import rospy
+from geometry_msgs.msg import Twist
 import std_msgs
 
 topics = namedtuple("Topics", ("camera", "clock", "drive", "plates"))(
@@ -13,15 +14,30 @@ topics = namedtuple("Topics", ("camera", "clock", "drive", "plates"))(
 
 def plate_message(location, plate_number):
     """Format a message for the license plate scorer"""
-    return "Hippos,1234,{},{}".format(location, plate_number)
+    return "Hippos,MitiFizz,{},{}".format(location, plate_number)
 
 
 if __name__ == "__main__":
     rospy.init_node("time_trials", anonymous=True)
     plates = rospy.Publisher(topics.plates, std_msgs.msg.String, queue_size=1)
+    drive = rospy.Publisher(topics.drive, Twist, queue_size=1)
 
     time.sleep(1)
 
     plates.publish(plate_message(0, "AA00"))
-    rospy.sleep(1)
+
+    move = Twist()
+    move.linear.x = 0.75
+    move.angular.z = 3
+    drive.publish(move)
+
+    rospy.sleep(0.5)
+    move.angular.z = 0
+    drive.publish(move)
+
+    rospy.sleep(1.5)
+    move.linear.x = 0
+    move.angular.z = 0
+    drive.publish(move)
+
     plates.publish(plate_message(-1, "AA00"))
